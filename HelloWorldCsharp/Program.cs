b1b833a16223809deb27d2165a655dac4723ea33
mlinaac <maep.linaac@coc.phinma.edu.ph>
2020-11-24 18:24:33 +0800

    init hello world

---
+++ HelloWorldCsharp/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: HORACE GRANT
+ * Date: 24/11/2020
+ * Time: 6:22 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace HelloWorldCsharp
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
